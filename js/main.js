da37b3ad394973dcd6a119b939cdb297a6f4c3c0
Michael Critz <Michael_Critz@playstation.sony.com>
2013-03-14 15:46:34 -0700

    Created JSON-ified data. Added ability to choose data set from dropdown.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,52 @@
-$('.card').drags();
+var cardDataSet;
+var url = 'data/data.json';
 
-$('.new_container').on(
-	'click', function(){
-		$('#main').append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>');
-	}
-);
 
-$('.close').on(
-	'click', function(){
-		$(this).parent().remove();
-});
 
-$('.destroy').on(
-	'click', function(){
-		$(this).parent().remove();
-});
+var clearCards = function(){
+	$('.card').remove();
+}
+
+var fetchData = function(url){
+	$.getJSON(url, function(data) {
+		cardDataSet = data;
+		return(cardDataSet);
+	});
+}
+
+var addCards = function(data, list) {
+	var cards = data[list];
+	$.each(cards, function(key,val) {
+		$('#main').append('<div class="card ' + key +'">' + val + '</div>');
+	});
+	$('.card').drags();
+}
+
+var init = function() {
+	fetchData(url);	
+	$('.new_container').on(
+		'click', function(){
+			$('#main').append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>');
+		}
+	);
+
+	$('.close').on(
+		'click', function(){
+			$(this).parent().remove();
+	});
+
+	$('.destroy').on(
+		'click', function(){
+			$(this).parent().remove();
+	});
+
+	$('#choose_card li a').on(
+		'click', function(){
+			var chosenItem = $(this).attr('id');
+			addCards(cardDataSet, chosenItem);
+		}
+	)
+
+}
+
+init();
