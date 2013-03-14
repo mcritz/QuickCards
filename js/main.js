761842047f23e7ec8f8c6c737bd1ea1451609b2c
Michael Critz <Michael_Critz@playstation.sony.com>
2013-03-14 15:57:48 -0700

    added clearElement function.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,12 @@ var cardDataSet;
 var url = 'data/data.json';
 
 
-
-var clearCards = function(){
-	$('.card').remove();
+// Utility method to cleanup the DOM
+var clearElement = function(selector){
+	$(selector).remove();
 }
 
+// Simple AJAX request
 var fetchData = function(url){
 	$.getJSON(url, function(data) {
 		cardDataSet = data;
@@ -14,38 +15,55 @@ var fetchData = function(url){
 	});
 }
 
+/*
+ *  Populate the field with cards from data list
+ *    @prop data = JSON object { "index": [array,of,cards] }
+ *    @prop list = chosen index
+ */
 var addCards = function(data, list) {
 	var cards = data[list];
 	$.each(cards, function(key,val) {
 		$('#main').append('<div class="card ' + key +'">' + val + '</div>');
 	});
+
+	/* 
+	 * $.drags is a seperate jQuery plugin.
+	 *    See js/vendor/drags.js
+	 */
 	$('.card').drags();
 }
 
 var init = function() {
-	fetchData(url);	
+	// Load data from JSON object
+	fetchData(url);
+
+	// Add new container from nav
 	$('.new_container').on(
 		'click', function(){
-			$('#main').append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>');
+			$('#main').append('<div class="row"><div class="group"><h3 contenteditable="true">container</h3></div></div>');
 		}
 	);
 
-	$('.close').on(
-		'click', function(){
-			$(this).parent().remove();
-	});
-
-	$('.destroy').on(
-		'click', function(){
-			$(this).parent().remove();
-	});
-
+	// Add cards based on #id from dropdown.
 	$('#choose_card li a').on(
 		'click', function(){
 			var chosenItem = $(this).attr('id');
 			addCards(cardDataSet, chosenItem);
 		}
 	)
+
+	$('#remove li a').on(
+		'click', function(){
+			var chosenItem = $(this).attr('id');
+			switch (chosenItem) {
+				case 'cards' :
+					clearElement('.card');
+					break;
+				case 'containers' :
+					clearElement('.group');
+					break;
+			}
+		})
 
 }
 
