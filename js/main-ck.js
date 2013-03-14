0c4ad2c08ad9d9a714443223e6f06611812cbb03
Michael Critz <Michael_Critz@playstation.sony.com>
2013-03-14 11:32:12 -0700

    Added drag behavior. Added some basic cards and containers.

---
+++ js/main-ck.js
@@ -0,0 +1,1 @@
+$(".card").drags();$(".new_container").on("click",function(){$("#main").append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>')});
--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
+$('.card').drags();
 
+$('.new_container').on(
+	'click', function(){
+		$('#main').append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>');
+	}
+);
