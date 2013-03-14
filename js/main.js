b484c524b39f9c93a4e4b45083c1cc2d48e4f764
Michael Critz <Michael_Critz@playstation.sony.com>
2013-03-14 16:05:46 -0700

    Refined data. Added ability to populate navigation on data load.

--- js/main.js
+++ js/main.js
@@ -11,8 +11,28 @@ var clearElement = function(selector){
 var fetchData = function(url){
 	$.getJSON(url, function(data) {
 		cardDataSet = data;
-		return(cardDataSet);
+		createNav(cardDataSet);
 	});
+}
+
+// Populate the dropdown based on returned data
+var createNav = function(navData){
+	$.each(navData, function(key,val){
+		$('#choose_card').append(
+            '<li><a id="' + key + '" href="#">' + key + '</a></li>'
+		)
+	})
+	initCardNav();
+}
+
+// Add cards based on #id from dropdown.
+var initCardNav = function(){
+	$('#choose_card li a').on(
+		'click', function(){
+			var chosenItem = $(this).attr('id');
+			addCards(cardDataSet, chosenItem);
+		}
+	)
 }
 
 /*
@@ -43,14 +63,6 @@ var init = function() {
 			$('#main').append('<div class="row"><div class="group"><h3 contenteditable="true">container</h3></div></div>');
 		}
 	);
-
-	// Add cards based on #id from dropdown.
-	$('#choose_card li a').on(
-		'click', function(){
-			var chosenItem = $(this).attr('id');
-			addCards(cardDataSet, chosenItem);
-		}
-	)
 
 	$('#remove li a').on(
 		'click', function(){
