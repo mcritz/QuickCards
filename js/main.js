82c3e4041b9671f5cfcd1c5a7cc209408bc528e3
Michael Critz <Michael_Critz@playstation.sony.com>
2013-03-14 11:52:52 -0700

    Added a diner.html example and cleaned up code.

--- js/main.js
+++ js/main.js
@@ -5,3 +5,13 @@ $('.new_container').on(
 		$('#main').append('<div class="row"><div class="drop_group"><h3 contenteditable="true">container</h3></div></div>');
 	}
 );
+
+$('.close').on(
+	'click', function(){
+		$(this).parent().remove();
+});
+
+$('.destroy').on(
+	'click', function(){
+		$(this).parent().remove();
+});
